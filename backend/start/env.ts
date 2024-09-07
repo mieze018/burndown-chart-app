31a533b6df5dd9a98fd43ebe1bf5f1c63e4b6638
Ayu Nakata <ayu.nakata@mugenup.com>
2024-09-07 23:09:53 +0900

    feat: node ace configure @a donisjs/redis

--- backend/start/env.ts
+++ backend/start/env.ts
@@ -35,4 +35,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
+
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional()
 })
