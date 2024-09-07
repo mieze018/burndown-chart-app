123e025a0878b82e3ee94a4b2535baa656494998
Ayu Nakata <ayu.nakata@mugenup.com>
2024-09-08 03:35:57 +0900

    style: run lint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
 import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
-import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
-import tailwind from 'tailwindcss'
+import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
