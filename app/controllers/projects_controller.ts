40571268dd090c21207a34134d9d520fb79fa7dc
Ayu Nakata <ayu.nakata@mugenup.com>
2024-09-14 11:09:34 +0900

    feat: Add project creation functionality to ProjectsController and index page

--- app/controllers/projects_controller.ts
+++ app/controllers/projects_controller.ts
@@ -2,30 +2,18 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProjectsController {
   async index({ inertia }: HttpContext) {
-    return inertia.render('projects/index')
+    return inertia.render('projects/index', {
+      projects: [
+        { id: 1, name: 'Project 1' },
+        { id: 2, name: 'Project 2' },
+        { id: 3, name: 'Project 3' },
+      ],
+    })
   }
 
-  async create({ inertia }: HttpContext) {
-    return inertia.render('projects/create')
-  }
-
-  async store({}: HttpContext) {
-    // Create a new project
-  }
-
-  async show({ inertia }: HttpContext) {
-    return inertia.render('projects/show')
-  }
-
-  async edit({ inertia }: HttpContext) {
-    return inertia.render('projects/edit')
-  }
-
-  async update({}: HttpContext) {
-    // Update the project
-  }
-
-  async destroy({}: HttpContext) {
-    // Delete the project
+  async post({ inertia, request, response }: HttpContext) {
+    debugger
+    const project = request.input('project')
+    response.redirect().toRoute('projects.index')
   }
 }
--- inertia/pages/projects/index.vue
+++ inertia/pages/projects/index.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-import { Link, Head } from '@inertiajs/vue3'
-const addProject = () => {
-  console.log('Add project')
-}
+import { Link, Head, useForm, useRemember } from '@inertiajs/vue3'
 import Button from '~/components/ui/button/Button.vue'
 import Input from '~/components/ui/input/Input.vue'
 import { ref } from 'vue'
+
 defineProps<{
   projects: Array<{ id: number; name: string }>
 }>()
 
-const project = ref({
-  id: 1,
-  name: 'Project 1',
+const newProject = ref({
+  name: '',
 })
+
+/** @see https://inertiajs.com/forms */
+const form = useForm(newProject.value)
+const handleSubmit = () => form.post('/projects/', { onSuccess: () => form.reset() })
 </script>
 
 <template>
   <Head title="Projects" />
 
-  <div v-for="project in projects" :key="project.id">
-    <Input v-model="project.name" />
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <Input v-model="newProject.name" />
+      <div v-if="form.errors.name">{{ form.errors.name }}</div>
 
-    <Button @click="addProject">Add Project</Button>
+      <Button type="submit" :disabled="form.processing">Add Project</Button>
+    </form>
+  </div>
+
+  <div v-for="project in projects" :key="project.id">
     <div>{{ project.id }}</div>
     <Link :href="`/projects/${project.id}`">
       {{ project.name }}
